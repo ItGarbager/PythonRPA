9d507e8bed2888963081e2744baec1b439ac4607
ItGarbager <linxuzhao2018@163.com>
2022-07-17 00:04:20 +0800

    :recycle: rewrite classes, compatible with Excel tupe arguments

--- claess.py
+++ claess.py
@@ -1,19 +1,47 @@
-from typing import Union
+import ast
+import logging
+from typing import Union, Any
 
 import pandas as pd
+import win32api
+import win32con
+
+logger = logging.getLogger(__name__)
 
 
 class Task:
     """自动化任务对象"""
 
-    def __init__(self, action, filepath, *args, **kwargs):
-        self.action = action
-        self.filepath = filepath
-        self.args = (self.action, self.filepath) + args
-        self.kwargs = kwargs
+    def __init__(self, action, image_path, *args, width=win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN),
+                 height=win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN),
+                 winname='match', threshold=.87, do_count=1, loop_limit=30,
+                 is_show=False, is_save=False,
+                 is_require=True, is_many=False):
+        """
+
+        :param action: 执行动作
+        :param image_path: 匹配图片路径
+        :param args: 传入动作的参数
+        :param width: 截取屏幕宽
+        :param height: 截取屏幕高
+        :param winname: 若展示匹配效果，展示时的屏幕名字
+        :param threshold: 匹配度阈值
+        :param do_count: 任务执行次数
+        :param loop_limit: 图片匹配次数
+        :param is_show: 是否展示匹配效果
+        :param is_save: 是否保存匹配图片
+        :param is_require: 图片匹配是否为必要条件
+        :param is_many: 同张截图中是否要匹配多张图片
+        """
+        local_arg = locals().copy()
+        local_arg.pop('self')
+        self.action = local_arg.pop('action')
+        self.image_path = local_arg.pop('image_path')
+        args = local_arg.pop('args')
+        self.args = (self.action, self.image_path) + args
+        self.kwargs = local_arg
 
     def get_data(self):
-
         if self.kwargs:
             return [self.args, self.kwargs]
         else:
@@ -55,51 +83,58 @@ class Action:
     sleep = 8
     exit = 9
     actions = 10
+    move = 11
 
 
 class Input:
     """根据传入参数构建 TaskList 生成器对象"""
     # 构建一个初始化的类型字典
     __action_dict = {
-        Action.nothing: ['', None, None],  # 未传入事件
-        Action.click_left_button: ['mouse_click', None, 'left', None],
-        Action.click_right_button: ['mouse_click', None, 'right', None],
+        Action.nothing: ['', None],  # 未传入事件
+        Action.click_left_button: ['mouse_click', None, 'left'],
+        Action.click_right_button: ['mouse_click', None, 'right'],
         Action.press_left_button: ['mouse_press', None, 'left'],
         Action.release_left_button: ['mouse_release', None, 'right'],
-        Action.scroll_button: ['mouse_scroll', None, 0, None],
-        Action.input_str: ['keyboard_type', None, None],
-        Action.press_keyboard: ['keyboard_group', None, None],
-        Action.sleep: ['sleep', None, None],
-        Action.exit: ['exit', None, None],
-        Action.actions: ['actions', None, None],
+        Action.scroll_button: ['mouse_scroll', None, 0],
+        Action.input_str: ['keyboard_type', None],
+        Action.press_keyboard: ['keyboard_group', None],
+        Action.sleep: ['sleep', None],
+        Action.exit: ['exit', None],
+        Action.actions: ['actions', None],
+        Action.move: ['mouse_move', None]
     }
 
     def __init__(
-            self, action: Union[Action, int, None] = None, arg=None, image_path=None,
+            self, action: Union[Action, int, None] = None, *args, image_path=None,
             excel_path='tasks.xlsx',
             **kwargs
     ):
+        # 判断是否仅传了一个参
+        if len(args) == 1:
+            args = args[0]
+            if isinstance(args, float):
+                int_arg = int(args)
+                args = (args == int_arg) and int_arg or args
+
+            args = (args,)
         if not action:
             action = Action.nothing
-        if isinstance(arg, float):
-            int_arg = int(arg)
-            arg = (arg == int_arg) and int_arg or arg
 
         if action == Action.actions:
             # 若为事件集类型则调用 get_xlsx_tasks 函数继续获取相应事件集的 TaskList 生成器对象
-            arg = get_xlsx_tasks(excel_path=excel_path, sheet_name=arg)
-
-        action = self.__action_dict.get(action)
+            args = (get_xlsx_tasks(excel_path=excel_path, sheet_name=args[0]),)
+        __action_dict = self.__action_dict.copy()
+        action = __action_dict.get(action)
         # 进行参数构建
-        if action[-1] is None:
-            action[-1] = arg
         action[1] = image_path
         action = tuple(action)
+        if args:
+            action += args
         # 构建任务对象
         self.task = Task(*action, **kwargs)
 
 
-def get_xlsx_tasks(excel_path='tasks.xlsx', sheet_name='main') -> TaskList:
+def get_xlsx_tasks(excel_path='tasks.xlsx', sheet_name: Any = 'main') -> TaskList:
     """
     在excel表中读取数据，构建 TaskList 生成器对象
     :param excel_path: excel 表格路径
@@ -107,28 +142,43 @@ def get_xlsx_tasks(excel_path='tasks.xlsx', sheet_name='main') -> TaskList:
     :return: TaskList 生成器对象
     """
     df = pd.read_excel(excel_path, names=[
-        'action', 'arg', 'image_path',
+        'action', 'args', 'image_path',
         'threshold', 'do_count', 'loop_limit',
         'is_require', 'is_many', 'is_show'
     ], sheet_name=sheet_name)  # 读取 excel 表格，并给字段命名
     fill_str = '1+-_-空数据-_-!￥%$&'  # 用以填充空数据，可以复杂一些
     df.fillna(fill_str, inplace=True)  # 填充缺失值
     kwargs_list = df.to_dict(orient='records')  # 将 dataframe 转为 dict 字典类型，并去除行索引
+
     for index, kwargs in enumerate(kwargs_list):
         kwargs_c = kwargs.copy()  # 复制一份用来进行字段处理
         for k, v in kwargs.items():
             if v == fill_str:  # 判断为空则删除该位置参
                 del kwargs_c[k]
+            else:
+                if k == 'args':
+                    try:
+                        v = ast.literal_eval(v)
+                    except ValueError:
+                        pass
+                    if not isinstance(v, tuple):
+                        v = (v,)
+                    kwargs_c[k] = v
+
         kwargs_list[index] = kwargs_c
 
     # 处理完成后构建任务列表
-    task_list = (
-        Input(
+    task_list = []
+    for kwargs in kwargs_list:
+        action = kwargs.pop('action')
+        args = kwargs.pop('args')
+        task_list.append(Input(
+            action,
+            *args,
             excel_path=excel_path,
             **kwargs
-        ).task
-        for kwargs in kwargs_list
-    )
+        ).task)
+
     # 传入当前任务列表构建 TaskList 生成器对象
     tasks = TaskList(
         *task_list
