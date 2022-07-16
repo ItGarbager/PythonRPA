0d34ff7136aaab98d7102f3e01b97300f993158d
ItGarbager <linxuzhao2018@163.com>
2022-07-17 00:48:32 +0800

    :art: update template

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import logging
 import warnings
 
-from classes import Task, TaskList, Input
+from classes import Task, TaskList, Input, Action
 from tools import do_tasks
 
 warnings.filterwarnings('ignore')
@@ -18,7 +18,7 @@ def test_action1():
         Task('mouse_click', None, 'right', 1).get_data(),  # 单击右键
         Task(None, 'images/test/0.png').get_data(),  # 移动至图片处
         Task('mouse_click', 'images/test/-1.png', 'left', 1).get_data(),  # 左键单击图所在的位置
-        Input(11, (100, 100), True).task.get_data(),  # 向下向右移动鼠标100
+        Input(Action.input.mouse_move, (100, 100), True).task.get_data(),  # 向下向右移动鼠标100
         ('mouse_click', 'images/test/1.png', 'right', 1),  # 右键单击图所在的位置
         ('mouse_click', 'images/test/2.png', 'left', 1),  # 左键单击图所在的位置
         ('keyboard_type', None, '6hikgkjkh'),  # 输入文本
@@ -39,30 +39,30 @@ def test_action1():
 
 def test_action2():
     tasks = TaskList(
-        Task('mouse_click', 'images/test2/1.png', 'left', 2),
-        Task('mouse_click', 'images/test2/2.png', 'left', 2),
-        Task('mouse_click', 'images/test2/3.png', 'left', 1),
-        Task('mouse_click', 'images/test2/4.png', 'left', 1),
-        Task('keyboard_type', None, 'bilibili'),
-        Task('keyboard_group', None, 'enter'),
-        Task('mouse_click', 'images/test2/5.png', 'left', 1),
-        Task('mouse_click', 'images/test2/6.png', 'left', 1, threshold=.92),
-        Task(None, 'images/test2/7.png', 'left'),
-        Task('sleep', None, 1.2),
-        Task('mouse_scroll', None, 0, -3900),
+        Task(Action.task.mouse_click, 'images/test2/1.png', 'left', 2),
+        Task(Action.task.mouse_click, 'images/test2/2.png', 'left', 2),
+        Task(Action.task.mouse_click, 'images/test2/3.png', 'left', 1),
+        Task(Action.task.mouse_click, 'images/test2/4.png', 'left', 1),
+        Task(Action.task.keyboard_type, None, 'bilibili'),
+        Task(Action.task.keyboard_group, None, 'enter'),
+        Task(Action.task.mouse_click, 'images/test2/5.png', 'left', 1),
+        Task(Action.task.mouse_click, 'images/test2/6.png', 'left', 1, threshold=.92),
+        Task(Action.task.nothing, 'images/test2/7.png', 'left'),
+        Task(Action.task.sleep, None, 1.2),
+        Task(Action.task.mouse_scroll, None, 0, -3900),
         Task(
-            'actions',
+            Action.task.actions,
             None,
             TaskList(
                 Task(
-                    'mouse_click', 'images/test2/11.png', 'left', 1,
+                    Action.task.mouse_click, 'images/test2/11.png', 'left', 1,
                     loop_limit=4, threshold=0.89,
                     is_require=False, is_many=True,
                     is_show=True,
                 ),
-                Task('sleep', None, 1.2),
-                Task('mouse_scroll', None, 0, -12),
-                Task('sleep', None, 1.2),
+                Task(Action.task.sleep, None, 1.2),
+                Task(Action.task.mouse_scroll, None, 0, -12),
+                Task(Action.task.sleep, None, 1.2),
             ),
             do_count=60
         ),
@@ -72,4 +72,4 @@ def test_action2():
 
 
 if __name__ == '__main__':
-    test_action1()
+    test_action2()
